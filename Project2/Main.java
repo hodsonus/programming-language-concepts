7d27e1e1d581ab32fe4025cfd097d3f1da2c9981
Christian Marin <christian.marin7@gmail.com>
2019-03-12 09:39:54 -0400

    Ruin build

---
+++ Project2/Main.java
@@ -0,0 +1,16 @@
+//import org.antlr.v4.runtime.ANTLRFileStream;
+//import org.antlr.v4.runtime.CommonTokenStream;
+//import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println();
+        //GrammarLexer lexer = new GrammarLexer(new ANTLRFileStream("test.mu"));
+        //GrammarParser parser = new GrammarParser(new CommonTokenStream(lexer));
+        /*
+        ParseTree tree = parser.parse();
+        EvalVisitor visitor = new EvalVisitor();
+        visitor.visit(tree);
+        */
+    }
+}
