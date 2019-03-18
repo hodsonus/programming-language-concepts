f41522d897a775fbc8aa1b9e34b52b944d403993
John Hodson <hodsonus@gmail.com>
2019-03-17 22:01:58 -0400

    a lot

--- Homework3/bc.ml
+++ Homework3/bc.ml
@@ -2,43 +2,51 @@ open Core
 open Stdlib
 
 (*
-TODO: questions
 why environment queue and not stack
 how to handle pre/post inc/dec ops
 how to handle mod
 how to read command line input
-
 is multiple params better than product type breakdown
 *)
 
 (* ============================== types ============================== *)
 
-type expr = (* a fragment of code evaluable to float, no state change *)
-| Num   of float
-| Var   of string
-| Op1   of string*expr
-| Op2   of string*expr*expr
+(* eval expr must return flaot,may return new state *)
+type expr = (* a fragment of code *)
+    | Num    of float
+    | Var    of string
+    | Op1    of string*expr
+    | Op2    of string*expr*expr
+    | Assign of string*expr
+    | FCall  of string*expr list
 
-type statement = (* a line of code, possible state change *)
-| Expr     of expr
-| Assign   of string*expr
-| If       of expr*statement list*statement list
-| While    of expr*statement list
-| For      of statement*expr*statement*statement list
-| FDef     of string*string list*statement list
-| FCall    of string*expr list
-| Return   of expr
-| Break    of unit
-| Continue of unit
-type block = (* a block of code, composition of statements *)
-(* | Block of *) statement list
+(* eval statement may return float and new state *)
+type statement = (* a line of code *)
+    | Expr     of expr
+    | If       of expr*statement list*statement list
+    | While    of expr*statement list
+    | For      of statement*expr*statement*statement list
+    | FDef     of string*string list*statement list
+    | Return   of expr
+    | Break    of unit
+    | Continue of unit
+
+type custExcept =
+    | ReturnInProgress of 
+    | BreakInProgress of unit
+    | ContinueInProgress of unit
+
+type block = (* a block of code *)
+    statement list
+type fxndef = (* a function definition *)
+    { name: string; blk:block; params:string list }
 
 type vars = (* a variable store for a given scope *)
-| Vars of (string, float)Hashtbl.t
+    (string,float)Hashtbl.t
 type fxns = (* a global store for function definitions *)
-(*| Fxns of *) (string, block)Hashtbl.t
+    (string,fxndef)Hashtbl.t
 type scopeStack = (* a stack maintaining variable store scopes *)
-(* | ScopeStack of *) vars list
+    (string,float)Hashtbl.t list
 
 (* ============================== functions ============================== *)
 
@@ -55,106 +63,190 @@ let rec evalVar (v: string) (ss: scopeStack) : float =
     match List.length ss with
     | 0 -> raise(Failure "Illegal variable environment state.")
     | 1 -> ( (* in global scope *)
-        match List.hd ss with
-        | Vars(vs) -> (
-            match Hashtbl.find_opt vs v with
-            | float -> (Hashtbl.find vs v) (* look in local (tail) *)
-            | None  -> ( (* call to look in global scope *)
-                let globalSS = Vars(vs)::[] in
-                evalVar v globalSS
-                )
-            )
+        let vs = List.hd ss in
+            let res_opt = Hashtbl.find_opt vs v in
+                match res_opt with
+                | Some(res) -> res
+                | None -> 0.
         )
     | _ -> ( (* not in global scope *)
-        match (List.nth ss ((List.length ss)-1)) with
-        | Vars(vs) -> (
-            match Hashtbl.find_opt vs v with
-            | float -> Hashtbl.find vs v (* look in global (head) *)
-            | None  -> 0.
-            )
+        let vs = List.nth ss ((List.length ss)-1) in
+            let res_opt = Hashtbl.find_opt vs v in
+                match res_opt with
+                | Some(res) -> res
+                | None -> (
+                    let globals = (List.hd ss)::[] in
+                        evalVar v globals
+                )
         )
 
-(* performs an expression evaluation given a program state *)
-let rec evalExpr (e: expr) (ss: scopeStack) (fs: fxns) : float =
+(* performs an expression evaluation *)
+let rec evalExpr (e: expr) (ss: scopeStack) (fs: fxns) (* : (float, scopeStack) *) =
     match e with
-    | Num(flt) -> flt
-    | Var(str) -> evalVar str ss
-    | Op1(op, ex) -> (
+    | Num(flt) -> flt,ss
+    | Var(str) -> (evalVar str ss),ss
+    | Op1(op,ex) -> (
         match op with
-        | "-" -> -.(evalExpr ex ss fs)
-        | "!" -> float_of_bool(not(bool_of_float(evalExpr ex ss fs)))
+        | "-" -> let v,ss1 = evalExpr ex ss fs in -.v,ss1
+        | "!" -> let v,ss1 = evalExpr ex ss fs in float_of_bool(not(bool_of_float(v))),ss1
         | _   -> raise(Failure ("Unknown unary operator `" ^ op ^ "`."))
         )
-    | Op2(op, expr1, expr2) -> (
+    | Op2(op,expr1,expr2) -> (
         match op with
-        | "+"  -> ((evalExpr expr1 ss fs) +. (evalExpr expr2 ss fs))
-        | "-"  -> ((evalExpr expr1 ss fs) -. (evalExpr expr2 ss fs))
-        | "*"  -> ((evalExpr expr1 ss fs) *. (evalExpr expr2 ss fs))
-        | "/"  -> ((evalExpr expr1 ss fs) /. (evalExpr expr2 ss fs))
-        | "^"  -> ((evalExpr expr1 ss fs) ** (evalExpr expr2 ss fs))
-        | "<=" -> float_of_bool ((evalExpr expr1 ss fs) <= (evalExpr expr2 ss fs))
-        | "<"  -> float_of_bool ((evalExpr expr1 ss fs) <  (evalExpr expr2 ss fs))
-        | ">=" -> float_of_bool ((evalExpr expr1 ss fs) >= (evalExpr expr2 ss fs))
-        | ">"  -> float_of_bool ((evalExpr expr1 ss fs) >  (evalExpr expr2 ss fs))
-        | "==" -> float_of_bool ((evalExpr expr1 ss fs) =  (evalExpr expr2 ss fs))
-        | "!=" -> float_of_bool ((evalExpr expr1 ss fs) <> (evalExpr expr2 ss fs))
-        | "&&" -> float_of_bool (
-            (bool_of_float (evalExpr expr1 ss fs)) && (bool_of_float (evalExpr expr2 ss fs)))
-        | "||" -> float_of_bool (
-            (bool_of_float (evalExpr expr1 ss fs)) || (bool_of_float (evalExpr expr2 ss fs)))
-        | "%"  -> float_of_int ( (* TODO verify casting solution is acceptable *)
-            int_of_float(evalExpr expr1 ss fs) % int_of_float(evalExpr expr1 ss fs))
+        | "+"  -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (val1 +. val2),ss2
+        )
+        | "-"  -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (val1 -. val2),ss2
+        )
+        | "*"  -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (val1 *. val2),ss2
+        )
+        | "/"  -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (val1 /. val2),ss2
+        )
+        | "%"  -> ((* TODO verify casting solution is acceptable *)
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (float_of_int(int_of_float(val1) % int_of_float(val2))),ss2
+        )
+        | "^"  -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (val1 ** val2),ss2
+        )
+        | "<=" -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (float_of_bool (val1 <= val2)),ss2
+        )
+        | "<"  -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (float_of_bool (val1 < val2)),ss2
+        )
+        | ">=" -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (float_of_bool (val1 >= val2)),ss2
+        )
+        | ">"  -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (float_of_bool (val1 > val2)),ss2
+        )
+        | "==" -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (float_of_bool (val1 = val2)),ss2
+        )
+        | "!=" -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (float_of_bool (val1 <> val2)),ss2
+        )
+        | "&&" -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (float_of_bool ((bool_of_float val1) && (bool_of_float val2))),ss2
+        )
+        | "||" -> (
+            let val1,ss1 = (evalExpr expr1 ss fs) in
+                let val2,ss2 = (evalExpr expr2 ss1 fs) in
+                    (float_of_bool ((bool_of_float val1) || (bool_of_float val2))),ss2
+        )
         | _    -> raise(Failure ("Unknown binary operator `" ^ op ^ "`."))
         )
+    | Assign(vName,expr) -> (
+        let vVal,ss1 = evalExpr expr ss fs in
+            Hashtbl.replace (List.nth ss1 (List.length ss1-1)) vName vVal;
+            vVal,ss1
+        )
+    | FCall(f,exprList) -> (
+        match Hashtbl.find_opt fs f with
+        | Some(fxn) -> evalFxn fxn exprList ss fs
+        | None -> (
+            match f with (* TODO *)
+            | "s"    -> (
+                match List.length exprList = 1 with
+                | true  -> let x,ss1 = evalExpr (List.hd exprList) ss fs in sin(x),ss1
+                | false -> raise(Failure "Sine function takes one argument."))
+            | "c"    -> (
+                match List.length exprList = 1 with
+                | true  -> let x,ss1 = evalExpr (List.hd exprList) ss fs in cos(x),ss1
+                | false -> raise(Failure "Cosine function takes one argument."))
+            | "e"    -> (
+                match List.length exprList = 1 with
+                | true  -> let x,ss1 = evalExpr (List.hd exprList) ss fs in exp(x),ss1
+                | false -> raise(Failure "Exp function takes one argument."))
+            | "l"    -> (
+                match List.length exprList = 1 with
+                | true  -> let x,ss1 = evalExpr (List.hd exprList) ss fs in log(x),ss1
+                | false -> raise(Failure "Log function takes one argument."))
+            | "sqrt" -> (
+                match List.length exprList = 1 with
+                | true  -> let x,ss1 = evalExpr (List.hd exprList) ss fs in sqrt(x),ss1
+                | false -> raise(Failure "Sqrt function takes one argument."))
+            | "read" -> (
+                match List.length exprList = 0 with
+                | true  -> ( let x = read_float() in x,ss )
+                | false -> raise(Failure "Read function takes no arguments."))
+            | _ -> raise(Failure ("Unknown function call to " ^ f ^ "."))
+        )
+    )
 
 (*  *)
-let evalBlock(blk: block) (ss: scopeStack) (fs: fxns) : unit =
-  (* TODO *)
-  (* create new environment *)
-  (* user fold_left *)
-  (* pop the local environment *)
-  print_endline "Not implemented"
+and evalFxn (fxn: fxndef) (el: expr list) (ss: scopeStack) (fs: fxns) (* : (float, scopeStack) *) =
+    let table = Hashtbl.create 10 in (* instantiate local scope *)
+        let ss1 = unpackArgs table (fxn.params) el ss fs in
+            let ss2 = ss1@[table] in (* add local scope to scope stack *)
+                evalBlock fxn.block ss2 fs (* call evalBlock to evaluate the body of the fxn. this throws (should) a return exception *)
+        
+    (* user fold_left *)
+    (* pop the local environment *)
+    0.,ss
+
+(* *)
+and unpackArgs (tbl: (string,float)Hashtbl.t) (params: string list) (el: expr list) (ss: scopeStack) (fs: fxns) : scopeStack =
+    match List.length params = List.length el with
+    | true  -> (
+        let x,ss1 = evalExpr (List.hd el) ss fs in
+            Hashtbl.add tbl (List.hd params) x;
+            unpackArgs tbl (List.tl params) (List.tl el) ss1 fs;
+            ss1
+    )
+    | false -> raise(Failure "Invalid number of arguments to custom function.")
 
 (*  *)
-let evalStatement (s: statement) (ss: scopeStack) (fs: fxns) : scopeStack =
+and evalBlock (blk: block) (ss: scopeStack) (fs: fxns) =
+    (* TODO *)
+    (* create new environment *)
+    (* user fold_left *)
+    (* pop the local environment *)
+    0.,ss,fs
+
+
+(*  *)
+let evalStatement (s: statement) (ss: scopeStack) (fs: fxns) (*float, scopeStack, fxns*) =
     match s with
-    | Expr(e) -> ( let ee = evalExpr e ss fs in ss ) (* TODO fix all these bullshit lines *)
-    | Assign(v, e) -> ( (*let ee = Hashtbl.replace (List.nth ss (List.length ss-1)) (evalExpr e ss fs) in*) ss )
-    | If(e, blkT, blkF) ->
+    | Expr(e) -> ( let ee = evalExpr e ss in ss ) (* TODO fix all these bullshit lines *)
+    | If(e,blkT,blkF) ->
         let x = (
         match bool_of_float(evalExpr e ss fs) with
         | true -> evalBlock blkT ss fs; ss
         | false -> evalBlock blkF ss fs; ss
         ) in ss
-    | While(e, blk) -> ss
-    | For(init, cond, term, blk) -> ss
-    | FDef(f, params, blk) -> ss
-    | FCall(f, exprList) -> (
-        match Hashtbl.find_opt fs f with
-        | block -> evalBlock (Hashtbl.find fs f) ss fs; ss
-        | None -> (
-            match f with (* TODO *)
-            | "s"    -> ( match List.length exprList == 1 with
-                    | true  -> let x = sin(evalExpr (List.hd exprList) ss fs) in ss
-                    | false -> raise(Failure "Sine function takes one argument."))
-            | "c"    -> ( match List.length exprList == 1 with
-                    | true  -> let x = cos(evalExpr (List.hd exprList) ss fs) in ss
-                    | false -> raise(Failure "Cosine function takes one argument."))
-            | "e"    -> ( match List.length exprList == 1 with
-                    | true  -> let x = exp(evalExpr (List.hd exprList) ss fs) in ss
-                    | false -> raise(Failure "Exp function takes one argument."))
-            | "l"    -> ( match List.length exprList == 1 with
-                    | true  -> let x = log(evalExpr (List.hd exprList) ss fs) in ss
-                    | false -> raise(Failure "Log function takes one argument."))
-            | "sqrt" -> ( match List.length exprList == 1 with
-                    | true  -> let x = sqrt(evalExpr (List.hd exprList) ss fs) in ss
-                    | false -> raise(Failure "Sqrt function takes one argument."))
-            | "read" -> ( match List.length exprList == 0 with
-                    | true  -> ( let x = read_float() in ss )
-                    | false -> raise(Failure "Read function takes no arguments."))
-            | _ -> raise(Failure ("Unknown function call to " ^ f ^ "."))
-            )
-        )
+    | While(e,blk) -> ss
+    | For(init,cond,term,blk) -> ss
+    | FDef(f,params,blk) -> ss (* TODO make sure in global scope*)
     | Return(e) -> ss
     | Break() -> ss
     | Continue() -> ss
@@ -162,7 +254,7 @@ let evalStatement (s: statement) (ss: scopeStack) (fs: fxns) : scopeStack =
 
 (*  *)
 let runCode(blk: block): unit = ()
-  (*evalBlock(param1, param2, ...)*)
+  (*evalBlock(param1,param2,...)*)
 
 (* ============================== tests ============================== *)
 
@@ -178,7 +270,7 @@ v = 10;
 v
 *)
 let p1: block = [
-  Assign("v", Num(1.0));
+  Assign("v",Num(1.0));
   Expr(Var("v"))
 ]
 let%expect_test "p1" =
@@ -196,11 +288,11 @@ else
 v
 *)
 let p2: block = [
-  Assign("v", Num(1.0));
-  If(Op2(">", Var("v"), Num(10.0)),
-    [Assign("v", Op2("+", Var("v"), Num(1.0)))],
-    [For(Assign("i", Num(2.0)), Op2("<", Var("i"), Num(10.0)), Expr(Op1("++a", Var("i"))),
-      [Assign("v", Op2("*", Var("v"), Var("i")))]
+  Assign("v",Num(1.0));
+  If(Op2(">",Var("v"),Num(10.0)),
+    [Assign("v",Op2("+",Var("v"),Num(1.0)))],
+    [For(Assign("i",Num(2.0)),Op2("<",Var("i"),Num(10.0)),Expr(Op1("++a",Var("i"))),
+      [Assign("v",Op2("*",Var("v"),Var("i")))]
     )]
   );
   Expr(Var("v"))
@@ -221,19 +313,19 @@ f(3)
 f(5)
 *)
 let p3: block = [
-  FDef("f", ["x"], [
-    If(Op2("<", Var("x"), Num(1.0)),
+  FDef("f",["x"],[
+    If(Op2("<",Var("x"),Num(1.0)),
       [Return(Num(1.0))],
       [Return(
         Op2("+",
-          FCall("f", [Op2("-", Var("x"), Num(1.0))]),
-          FCall("f", [Op2("-", Var("x"), Num(1.0))])
+          FCall("f",[Op2("-",Var("x"),Num(1.0))]),
+          FCall("f",[Op2("-",Var("x"),Num(1.0))])
         )
       )]
     )]
   );
-  Expr(FCall("f", [Num(3.0)]));
-  Expr(FCall("f", [Num(5.0)]));
+  Expr(FCall("f",[Num(3.0)]));
+  Expr(FCall("f",[Num(5.0)]));
 ]
 let%expect_test "p3" =
   evalBlock p3 []; (* TODO use runCode? *)
