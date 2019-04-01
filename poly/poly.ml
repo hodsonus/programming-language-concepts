7213d6d4390c1a03b16a8781c2c9f6040dd1021c
Christian Marin <christian.marin7@gmail.com>
2019-04-01 10:36:13 -0400

    from_expr, degree done; print_pExp in progress

--- poly/poly.ml
+++ poly/poly.ml
@@ -1,61 +1,139 @@
 (* Sum type to encode efficiently polynomial expressions *)
 type pExp =
-  | Term of int*int (*
-      First int is the constant
-      Second int is the power of x 
-      10  -> Term(10,0)
-      2x -> Term(2,1)
-      3x^20 -> Term(3, 20)
+  | Term of int*int
+    (* First int is the constant
+       Second int is the power of x
+       10  -> Term(10,0)
+       2x -> Term(2,1)
+       3x^20 -> Term(3, 20)
     *)
   | Plus of pExp list
-  (*
-    List of terms added
-    Plus([Term(2,1); Term(1,0)])
-  *)
+    (* List of terms added Plus([Term(2,1); Term(1,0)]) *)
   | Times of pExp list (* List of terms multiplied *)
 
+(* Function to traslate betwen AST expressions to pExp expressions *)
+let rec from_expr (_e: Expr.expr) : pExp =
+    match _e with
+        | Num(num) -> (
+            Term(num,0)
+        )
+        | Var(var) -> (
+            match var with
+                | 'x' -> Term(1,1)
+                | _ -> raise(Failure("error unknown variable"))
+        )
+        | Add(exp1, exp2) -> (
+            Plus([from_expr exp1; from_expr exp2])
+        )
+        | Sub(exp1, exp2) -> (
+            Plus([from_expr exp1; Times([Term(-1,0); from_expr exp2])])
+        )
+        | Mul(exp1, exp2) -> (
+            Times([from_expr exp1; from_expr exp2])
+        )
+        | Pow(exp, num) -> (
+            Times(pow_expr [] (from_expr exp) num)
+        )
+        | Pos(exp) -> (
+            from_expr exp
+        )
+        | Neg(exp) -> (
+            Times([Term(-1,0); from_expr exp])
+        )
 
-(*
-  Function to traslate betwen AST expressions
-  to pExp expressions
+and pow_expr(accum: pExp list) (e: pExp) (n: int) : (pExp list) =
+    match n with
+        | 0 -> accum
+        | _ -> pow_expr (accum@[e]) e (n-1)
+
+(*Compute degree of a polynomial expression.
+    Hint 1: Degree of Term(n,m) is m
+    Hint 2: Degree of Plus[...] is the max of the degree of args
+    Hint 3: Degree of Times[...] is the sum of the degree of args
 *)
-let from_expr (_e: Expr.expr) : pExp =
-    Term(0,0) (* TODO *)
+let rec degree (_e:pExp): int =
+    match _e with
+        | Term(n,m) -> m
+        | Plus(lis) -> deg_plus lis 0
+        | Times(lis) -> deg_times lis 0
 
-(* 
-  Compute degree of a polynomial expression.
+and deg_plus (lis : pExp list) (curMax : int) : int =
+    match lis with
+        | [] -> curMax
+        | hd::tl -> (
+            match hd with
+                | Term(n,m) -> max curMax m
+                | Plus(l) -> max curMax (deg_plus l 0)
+                | Times(l) -> max curMax (deg_times l 0)
+        )
 
-  Hint 1: Degree of Term(n,m) is m
-  Hint 2: Degree of Plus[...] is the max of the degree of args
-  Hint 3: Degree of Times[...] is the sum of the degree of args 
-*)
-let degree (_e:pExp): int = 0 (* TODO *)
+and deg_times (lis : pExp list) (accum : int) : int =
+    match lis with
+        | [] -> accum
+        | hd::tl -> (
+            match hd with
+                | Term(n,m) -> accum + m
+                | Plus(l) -> accum + (deg_plus l 0)
+                | Times(l) -> accum + (deg_times l 0)
+        )
 
-(* 
-  Comparison function useful for sorting of Plus[..] args 
-  to "normalize them". This way, terms that need to be reduced
-  show up one after another.
+(* Comparison function useful for sorting of Plus[..] args to "normalize
+    them". This way, terms that need to be reduced show up one after another.
   *)
 let compare (e1: pExp) (e2: pExp) : bool =
   degree e1 > degree e2
 
-(* Print a pExpr nicely 
+(* Print a pExpr nicely
   Term(3,0) -> 3
-  Term(5,1) -> 5x 
+  Term(5,1) -> 5x
   Term(4,2) -> 4x^2
-  Plus... -> () + () 
+  Plus... -> () + ()
   Times ... -> ()() .. ()
 
-  Hint 1: Print () around elements that are not Term() 
+  Hint 1: Print () around elements that are not Term(
   Hint 2: Recurse on the elements of Plus[..] or Times[..]
 *)
-let print_pExp (_e: pExp): unit =
-  (* TODO *)
-  Printf.printf("Not implemented");
-  print_newline()
+let rec print_pExp (_e: pExp) : unit =
+    match _e with
+        | Term(n, m) -> (
+            match m == 0 with
+                | true -> (
+                    Printf.printf("%d", n);
+                    ()
+                )
+                | false -> (
+                    match n with
+                        | 0 -> ( Printf.printf("0"); () )
+                        | 1 -> ();
+                        | _ -> Printf.printf(string_of_int(n));
+                    match m with
+                        | 1 -> Printf.printf("x");
+                        | _ -> Printf.printf("x^"^string_of_int(m));
+                )
+        )
+        | Plus(l) -> (
+            print '(';
+            print_plus l;
+            print ')';
+            ()
+        )
+        | Times(l) ->  (
+            print '(';
+            print_times l;
+            print ')';
+            ()
+        )
 
-(* 
-  Function to simplify (one pass) pExpr
+and print_plus (lis: pExp list) : unit =
+    match lis with
+        | [] -> ()
+        | hd::tl -> (
+            ()
+        )
+
+and print_times (lis: pExp list) : unit = ()
+
+(*Function to simplify (one pass) pExpr
 
   n1 x^m1 * n2 x^m2 -> n1*n2 x^(m1+m2)
   Term(n1,m1)*Term(n2,m2) -> Term(n1*n2,m1+m2)
@@ -66,7 +144,7 @@ let print_pExp (_e: pExp): unit =
   Hint 4: Accumulate terms. Term(n1,m)+Term(n2,m) => Term(n1+n2,m)
           Term(n1, m1)*Term(n2,m2) => Term(n1*n2, m1+m2)
   Hint 5: Use distributivity, i.e. Times[Plus[..],] => Plus[Times[..],]
-    i.e. Times[Plus[Term(1,1); Term(2,2)]; Term(3,3)] 
+    i.e. Times[Plus[Term(1,1); Term(2,2)]; Term(3,3)]
       => Plus[Times[Term(1,1); Term(3,3)]; Times[Term(2,2); Term(3,3)]]
       => Plus[Term(2,3); Term(6,5)]
   Hint 6: Find other situations that can arise
@@ -74,25 +152,19 @@ let print_pExp (_e: pExp): unit =
 let simplify1 (e:pExp): pExp =
     e
 
-(* 
-  Compute if two pExp are the same 
-  Make sure this code works before you work on simplify1  
+(*Compute if two pExp are the same
+  Make sure this code works before you work on simplify1
 *)
 let equal_pExp (_e1: pExp) (_e2: pExp) :bool =
   true
 
-(* Fixed point version of simplify1 
-  i.e. Apply simplify1 until no 
-  progress is made
-*)    
+(* Fixed point version of simplify1
+  i.e. Apply simplify1 until no progress is made
+*)
 let rec simplify (e:pExp): pExp =
     let rE = simplify1(e) in
       print_pExp rE;
       if (equal_pExp e rE) then
         e
-      else  
+      else
         simplify(rE)
-
-
-
-
