fbc68230724af390a0b21af807a8c880995f9b7c
John Hodson <hodsonus@gmail.com>
2019-04-21 18:03:17 -0400

    for statements, local variables remaining

--- typeInf/typeInf.pl
+++ typeInf/typeInf.pl
@@ -1,12 +1,11 @@
-% there is a debug mode - this changes the way that prolog compiles and then interprets the code
-% prolog USUALLY compiles the code after optimizing it
-% some of the code that is given below is a little bit subtle - some is not subtle
+% loops discouraged, use trace for debugging, reload code often
 
-% asserts
-    % 'asserta' adds a new definition for whatever you give - almoset behaves as a macro
-        % adds a new definition to gvars - becomes the most important one
-        % runs on unification - this system takes the place of hash tables in OCaml
-        % line 79 sets up the whole system, the call here prevents exceptions from being thrown when it sees no Gvars
+% 'asserta' adds a new definition to gvars with first priority
+% 'assertz' adds a new definition to gvars with last priority
+% both run on unification - and take the place of hash tables in OCaml
+% deleteGVars - sets up the whole system, the call here prevents exceptions from being thrown when it sees no Gvars
+
+% NOTE: use nondet as option to test if the test is nondeterministic
 
 % infer cleans
     % makes sure the code is a list
@@ -17,18 +16,12 @@
 % we have to write the code to define fucntions
 % test the functons in a specific way - add a fact with a name (my_funct, for example) and the input type to the output type
 
-% what he wants us to go and do
-    % figure out how you can represent every type of statement - this is the equivalent of sum types in OCaml'
-        % this is necessary for the bonus
-
-% strong advice to us
-%     **reload the code**
+% figure out how you can represent every type of statement - this is the equivalent of sum types in OCaml'
+% this is necessary for the bonus
 
 % be aware of the following
 %     define some fxns that dont need to bind variables - let them work in a generic way
 %     in Ocaml, the type infrerence mechanism can say a' -> a' (alpha type to alpha type)
-
-% similarly to OCaml, loops are discouraged heavily
 
 % the only way to get things done here is through unification
 
@@ -42,70 +35,84 @@
 % 	declare as dynamic ??
 % 	dynamic name_of_predicate
 
-% use **trace**, with or without the tests
-
-/* match functions by unifying with arguments 
-    and infering the result
+/*
+    as you encounter global variable definitions
+    or global functions add their definitions to the database using:
+        asserta( gvar(Name, Type) )
+    To check the types as you encounter them in the code use:
+        gvar(Name, Type) with the Name bound to the name.
+    Type will be bound to the global type
+    Examples:
+        g
+    Call the predicate deleveGVars() to delete all global
+    variables. Best wy to do this is in your top predicate
 */
+
+/* match functions by unifying with arguments and infering the result */
 typeExp(Fct, T):-
-    \+ var(Fct), /* make sure Fct is not a variable */ 
+    \+ var(Fct), /* make sure Fct is not a variable */
     \+ atom(Fct), /* or an atom */
     functor(Fct, Fname, _Nargs), /* ensure we have a functor */
     !, /* if we make it here we do not try anything else */
     Fct =.. [Fname|Args], /* get list of arguments */
-    append(Args, [T], FType), /* make it loook like a function signature */
+    append(Args, [T], FType), /* make it look like a function signature */
     functionType(Fname, TArgs), /* get type of arguments from definition */
     typeExpList(FType, TArgs). /* recurisvely match types */
 
 /* propagate types */
 typeExp(T, T).
 
-/* list version to allow function mathine */
+/* list version to allow function matching */
 typeExpList([], []).
 typeExpList([Hin|Tin], [Hout|Tout]):-
     typeExp(Hin, Hout), /* type infer the head */
     typeExpList(Tin, Tout). /* recurse */
 
-/* TODO: add statements types and their type checking */
-
+/* list version to allow block matching */
+typeStatementList([], []).
+typeStatementList([Hin|Tin], [Hout|Tout]):-
+    typeStatement(Hin, Hout), /* type infer the head */
+    typeStatementList(Tin, Tout). /* recurse */
 
 /* global variable definition
     Example:
         gvLet(v, T, int) ~ let v = 3;
  */
-typeStatement(gvLet(Name, T, Code), unit):-
+typeStatement(gvLet(Name, T, Code), unit) :-
     atom(Name), /* make sure we have a bound name */
     typeExp(Code, T), /* infer the type of Code and ensure it is T */
     bType(T), /* make sure we have an infered type */
     asserta(gvar(Name, T)). /* add definition to database */
 
-% TODO, extra credit
-% all you need to do to define the if statement\
+/* Expressions are statements */
+typeStatement(Expr, T) :-
+    typeExp(Expr,T).
 
-% this is a super generic data structure
-% similar to tuples in other proogramming langeages
-
-% because we have thsi suepr general structure, so long
-% as we have stuff that recognizes things that can fit
-% into this general data structure then we are good
-
-% the only reason the f types and the b types are
-% designed are to organize the structure of the program better 
 typeStatement(if(Cond, TCode, FCode),T) :-
     typeExp(Cond, bool),
     typeCode(TCode, T),
     typeCode(FCode, T),
     bType(T).
 
-/* Expressions are statements */
-typeStatement(Expr, T) :-
-    typeExp(Expr,T).
+% an if with no else
+typeStatement(if(Cond, TCode),T) :-
+    typeExp(Cond, bool),
+    typeCode(TCode, T),
+    bType(T).
 
-typeCode([],T) :- bType(T). % enforces no type for empty lists, a generic alpha type
+typeStatement(while(Cond, Code),T) :-
+    typeExp(Cond, bool),
+    typeCode(Code, T),
+    bType(T).
 
-/* Code is simply a list of statements. The type is 
-    the type of the last statement 
-*/
+typeStatement(for(Init, Cond, End, Code),T) :-
+    typeStatement(Init, unit),
+    typeExp(Cond, bool),
+    typeStatement(End, unit),
+    typeCode(Code, T),
+    bType(T).
+
+/* Code is simply a list of statements. The type is the type of the last statement */
 typeCode([S], T):-typeStatement(S, T).
 typeCode([S, S2|Code], T):-
     typeStatement(S,_T),
@@ -117,58 +124,55 @@ infer(Code, T) :-
     deleteGVars(), /* delete all global definitions */
     typeCode(Code, T).
 
-/* Basic types
-    TODO: add more types if needed
- */
+/* Basic types */
 bType(bool).
 bType(int).
 bType(float).
 bType(string).
-bType(unit). /* unit type for things that are not expressions */
-/*  functions type.
-    The type is a list, the last element is the return type
-    E.g. add: int->int->int is represented as [int, int, int]
-    and can be called as add(1,2)->3
- */
-bType([H]):- bType(H).
-bType([H|T]):- bType(H), bType(T).
+bType(unit).
 
-% allow alpha types
-% with this, go into the if statements and ensure that we do not have free vars
-bType(T) :-
-    var(T).
+/* Alpha types */
+bType(T) :- var(T).
 
-/*
-    TODO: as you encounter global variable definitions
-    or global functions add their definitions to 
-    the database using:
-        asserta( gvar(Name, Type) )
-    To check the types as you encounter them in the code
-    use:
-        gvar(Name, Type) with the Name bound to the name.
-    Type will be bound to the global type
-    Examples:
-        g
+/* Functions type */
+% The type is a list, the last element is the return type and can be called as add(1,2)->3
+% E.g. add: int->int->int is represented as [int, int, int]
+bType([H]) :- bType(H).
+bType([H|T]) :- bType(H), bType(T).
 
-    Call the predicate deleveGVars() to delete all global 
-    variables. Best wy to do this is in your top predicate
-*/
-
-deleteGVars():-
+deleteGVars() :-
     retractall(gvar),
     asserta(gvar(_X,_Y) :- false()).
 
-/*  builtin functions
-    Each definition specifies the name and the 
-    type as a function type
+fType(equal, [T, T, bool]).
+fType(nequal, [T, T, bool]).
+fType(lesser, [T, T, bool]).
+fType(lesserEq, [T, T, bool]).
+fType(greater, [T, T, bool]).
+fType(greaterEq, [T, T, bool]).
 
-    TODO: add more functions
-*/
-fType('<', [float, float, bool]).
-fType(iplus, [int,int,int]).
-fType(fplus, [float, float, float]).
-fType(fToInt, [float,int]).
-fType(iToFloat, [int,float]).
+fType(bAnd, [bool, bool, bool]).
+fType(bOr, [bool, bool, bool]).
+fType(bNot, [bool, bool]).
+
+fType(iNeg, [int, int]).
+fType(fNeg, [float, float]).
+
+fType(iPlus, [int, int, int]).
+fType(fPlus, [float, float, float]).
+fType(iSub, [int, int, int]).
+fType(fSub, [float, float, float]).
+fType(iTimes, [int, int, int]).
+fType(fTimes, [float, float, float]).
+fType(iDivide, [int, int, int]).
+fType(fDivide, [float, float, float]).
+fType(iMod, [int, int, int]).
+fType(fExp, [float, float, float]).
+fType(fToInt, [float, int]).
+fType(iToFlt, [int, float]).
+
+fType(sConcat, [string, string, string]).
+
 fType(print, [_X, unit]). /* simple print */
 fType(identity, [T,T]). /* polymorphic function */
 /* Find function signature
--- typeInf/typeInf.plt
+++ typeInf/typeInf.plt
@@ -1,7 +1,7 @@
 :- begin_tests(typeInf).
-:- include(typeInf). 
+:- include(typeInf).
 
-/* Note: when writing tests keep in mind that 
+/* Note: when writing tests keep in mind that
     the use of of global variable and function definitions
     define facts for gvar() predicate. Either test
     directy infer() predicate or call
@@ -9,28 +9,25 @@
 */
 
 % tests for typeExp
-test(typeExp_iplus) :- 
-    typeExp(iplus(int,int), int).
-
-% this test should fail
-test(typeExp_iplus_F, [fail]) :-
-    typeExp(iplus(int, int), float).
+test(typeExp_iplus) :-
+    typeExp(iPlus(int,int), int).
 
 test(typeExp_iplus_T, [true(T == int)]) :-
-    typeExp(iplus(int, int), T).
+    typeExp(iPlus(int, int), T).
 
-% NOTE: use nondet as option to test if the test is nondeterministic
+test(typeExp_iplus_F, [fail]) :- % should fail
+    typeExp(iPlus(int, int), float).
 
 % test for statement with state cleaning
 test(typeStatement_gvar, [nondet, true(T == int)]) :- % should succeed with T=int
-    deleteGVars(), /* clean up variables */
-    typeStatement(gvLet(v, T, iplus(X, Y)), unit),
+    deleteGVars(), % clean up variables
+    typeStatement(gvLet(v, T, iPlus(X, Y)), unit),
     assertion(X == int), assertion( Y == int), % make sure the types are int
     gvar(v, int). % make sure the global variable is defined
 
-% same test as above but with infer 
+% same test as above but with infer
 test(infer_gvar, [nondet]) :-
-    infer([gvLet(v, T, iplus(X, Y))], unit),
+    infer([gvLet(v, T, iPlus(X, Y))], unit),
     assertion(T==int), assertion(X==int), assertion(Y=int),
     gvar(v,int).
 
@@ -40,5 +37,43 @@ test(mockedFct, [nondet]) :-
     asserta(gvar(my_fct, [int, float])), % add my_fct(int)-> float to the gloval variables
     typeExp(my_fct(X), T), % infer type of expression using or function
     assertion(X==int), assertion(T==float). % make sure the types infered are correct
+
+test(ifStatement, [nondet]) :-
+    typeStatement(if(lesser(X,float), [int], [int]), T),
+    assertion(X==float), assertion(T==int).
+
+test(whileStatement, [nondet]) :-
+    typeStatement(while(greater(float,float), [bool]), T),
+    assertion(T==bool).
+
+test(gvLetInt, [nondet]) :-
+    deleteGVars(),
+    typeStatement(gvLet(i, TAssign, iPlus(int, int)), T),
+    assertion(T==unit), assertion(TAssign==int).
+
+test(gvLetFloat, [nondet]) :-
+    deleteGVars(),
+    typeStatement(gvLet(i, TAssign, fPlus(float, float)), T),
+    assertion(T==unit), assertion(TAssign==float).
+
+test(t5, [nondet]) :-
+    deleteGVars(),
+    typeStatement(unit, T),
+    assertion(T==unit).
+
+test(forStatement, [nondet]) :-
+    deleteGVars(),
+    typeStatement(for(unit, bool, unit, [bool]), T),
+    assertion(T==bool).
+
+test(t7, [nondet]) :-
+    deleteGVars(),
+    typeStatement(
+        for(gvLet(i, TAssign, fPlus(float, float)), 
+            greater(float,float),
+            gvLet(j, TAssign, fPlus(float, float)),
+            [bool]),
+        T2),
+    assertion(TAssign==float), assertion(T2==bool).
 
 :-end_tests(typeInf).
